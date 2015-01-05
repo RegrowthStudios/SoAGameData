46a6c513b789b2eee0ae08b1a2b5ec1d5ce4b7b1
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2015-01-05 02:13:49 +0000

    Styling cleanup and generalisation, applied initially to index of main menu also. Added generateText and generateLink for link and text items in menu lists.

--- UI/MainMenu/scripts/menu.js
+++ UI/MainMenu/scripts/menu.js
@@ -8,21 +8,123 @@ $(document).ready(function () {
     });
 });
 
-// Handles menu item hover
-$(document).ready(function () {
-    $(".menu-item").mouseenter(function () {
-        $(this).children(".hover")[0].play();
+//// Handles menu item hover
+//$(document).ready(function () {
+//    $(".menu-item").mouseenter(function () {
+//        $(this).children(".hover")[0].play();
+//    });
+//});
+
+//// Handles sub menu expand/contract
+//$(document).ready(function () {
+//    $(".menu-item-expandable").click(function () {
+//        if ($(this).hasClass("expanded")) {
+//            $(this).children(".contract")[0].play();
+//        } else {
+//            $(this).children(".expand")[0].play();
+//        }
+//        $(this).toggleClass("expanded");
+//    });
+//});
+
+var opts;
+var optDescs;
+// Handle item descriptions.
+function refreshDescControl() {
+    opts.off('mouseover mouseout');
+    opts.on('mouseover', function () {
+        var midId = $(this).data("mid-id");
+        for (var x = 0; x < opts.length; ++x) {
+            if ($(optDescs[x]).data("mid-id") == midId) {
+                $(optDescs[x]).show(0);
+            } else {
+                $(optDescs[x]).stop(false, true);
+            }
+        }
+    })
+    opts.on('mouseout', function () {
+        var midId = $(this).data("mid-id");
+        for (var x = 0; x < opts.length; ++x) {
+            if ($(optDescs[x]).data("mid-id") == midId) {
+                $(optDescs[x]).hide(0);
+            } else {
+                $(optDescs[x]).stop(false, true);
+            }
+        }
     });
+}
+
+//Resize scrollable panes to corret height.
+$(document).ready(function () {
+    if ($(".scrollable-pane.full-height").length > 0) {
+        setTimeout(function () {
+            var h = $(window).height();
+            var r = $("#menu-header").height();
+            r += parseInt($("#outer-wrapper").css('marginTop'));
+            r += $(".header").outerHeight(true);
+            r += parseInt($("#options").css('marginTop'));
+            r += 60; //Buffer at bottom.
+            h -= r;
+            $("#options").height(h);
+        }, 10);
+    }
 });
 
-// Handles sub menu expand/contract
-$(document).ready(function () {
-    $(".menu-item-expandable").click(function () {
-        if ($(this).hasClass("expanded")) {
-            $(this).children(".contract")[0].play();
+/**
+ * Creates a category for the options menu.
+ * name - Name of the category to be displayed to the user.
+ */
+function createCategory(name) {
+    var cid = name.replace(" ", "-");
+    //<li class="header" data-category="disp">Display Options</li>
+    var htmlCategory = "<li class='list-header' data-category='" + cid + "'>" + name + "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div></li>";
+    $("#options").append(htmlCategory);
+}
+
+function placeControl(cid, htmlControl) {
+    var cat = $(".list-header[data-category=" + cid + "]");
+    if (cat.length == 0) {
+        $("#options").append(htmlControl);
+    } else {
+        var elems = cat.nextUntil(".list-header");
+        if (elems.length == 0) {
+            cat.after(htmlControl);
         } else {
-            $(this).children(".expand")[0].play();
+            elems.last().after(htmlControl);
         }
-        $(this).toggleClass("expanded");
-    });
-});
+    }
+}
+function addDescription(oid, description) {
+    if (typeof description == "string") {
+        var htmlDescriptor = "<div class='helper' data-mid-id='" + oid + "'>" + description + "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div></div>"
+        $(htmlDescriptor).appendTo("#options-helper-wrapper").hide();
+        optDescs = $("#options-helper-wrapper > div");
+        opts = $("#options > .list-item");
+        refreshDescControl();
+    }
+}
+
+function generateLink(name, link, category, description) {
+    var oid = name.replace(" ", "-");
+    var cid = category.replace(" ", "-");
+    var htmlControl = "<li class='list-item clickable row' data-mid-id='" + oid + "'>";
+    htmlControl += "<a href='" + link + "'>";
+    htmlControl += name;
+    htmlControl += "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div>";
+    htmlControl += "</a>";
+    htmlControl += "</li>";
+    placeControl(cid, htmlControl);
+    addDescription(oid, description);
+}
+
+function generateText(name, text, category, description) {
+    var oid = name.replace(" ", "-");
+    var cid = category.replace(" ", "-");
+    var htmlControl = "<li class='list-item row' data-mid-id='" + oid + "'>";
+    htmlControl += "<div class='column-2'>" + name + "</div>";
+    htmlControl += "<div class='content-center column-2'>" + text + "</div>";
+    htmlControl += "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div>";
+    htmlControl += "</li>";
+    placeControl(cid, htmlControl);
+    addDescription(oid, description);
+}
--- UI/MainMenu/scripts/options.js
+++ UI/MainMenu/scripts/options.js
@@ -1,17 +1,4 @@
-﻿//Generate Controls
-
-/**
- * Creates a category for the options menu.
- * name - Name of the category to be displayed to the user.
- */
-function createCategory(name) {
-    var cid = name.replace(" ", "-");
-    //<li class="header" data-category="disp">Display Options</li>
-    var htmlCategory = "<li class='option-category' data-category='" + cid + "'>" + name + "</li>";
-    $(".options").append(htmlCategory);
-}
-
-var controls = [];
+﻿var controls = [];
 
 /**
  * Creates a toggle control for the options menu.
@@ -19,16 +6,16 @@ var controls = [];
  * name - Name of the control to be displayed to the user.
  * initialVal - The initial value of the toggle. Takes either an empty string or "checked".
  * updateInRealTime - Should changes to this control be reflected in-game immediately?
- * description - The description to be displayed, describing the controls effect on the game.
+ * description - The description to be displayed, describing the control's effect on the game.
  * category - The category to place the control under.
  */
-function generateToggle(ID, name, initialVal, updateInRealTime, description, category) {
+function generateToggle(ID, name, initialVal, updateInRealTime, category, description) {
     controls[ID] = initialVal;
     var oid = name.replace(" ", "-");
     var cid = category.replace(" ", "-");
-    var htmlControl = "<li class='option row clearfix' data-mid-id='" + oid + "'>";
-    htmlControl += "<div class='control half-column'>" + name + "</div>";
-    htmlControl += "<div class='control-value half-column'>";
+    var htmlControl = "<li class='list-item row' data-mid-id='" + oid + "'>";
+    htmlControl += "<div class='column-2'>" + name + "</div>";
+    htmlControl += "<div class='content-center column-2'>";
     htmlControl += "<div class='checkbox' data-id='" + ID + "'>";
     htmlControl += "<input id='" + oid + "' type='checkbox' name='" + oid + "'" + initialVal + "'" + (updateInRealTime ? " oninput='handleToggleChange(value, " + ID + ")'" : "") + ">";
     htmlControl += "<label for='" + oid + "'></label>";
@@ -36,18 +23,8 @@ function generateToggle(ID, name, initialVal, updateInRealTime, description, cat
     htmlControl += "</div>";
     htmlControl += "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div>";
     htmlControl += "</li>";
-    var cat = $(".option-category[data-category=" + cid + "]");
-    var elems = cat.nextUntil(".option-category");
-    if (elems.length == 0) {
-        cat.after(htmlControl);
-    } else {
-        elems.last().after(htmlControl);
-    }
-    var htmlDescriptor = "<div class='helper' data-mid-id='" + oid + "'>" + description + "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div></div>"
-    $(htmlDescriptor).appendTo(".options-helper-wrapper").hide();
-    optDescs = $(".options-helper-wrapper > div");
-    opts = $(".options > .option");
-    refreshDescControl();
+    placeControl(cid, htmlControl);
+    addDescription(oid, description);
 }
 
 
@@ -60,33 +37,23 @@ function generateToggle(ID, name, initialVal, updateInRealTime, description, cat
  * initialVal - The initial value of the slider. Takes a value between min and max.
  * intervalRes - The size of each interval in the slider.
  * updateInRealTime - Should changes to this control be reflected in-game immediately?
- * description - The description to be displayed, describing the controls effect on the game.
+ * description - The description to be displayed, describing the control's effect on the game.
  * category - The category to place the control under.
  */
-function generateSlider(ID, name, min, max, initialVal, intervalRes, updateInRealTime, description, category) {
+function generateSlider(ID, name, min, max, initialVal, intervalRes, updateInRealTime, category, description) {
     controls[ID] = initialVal;
     var oid = name.replace(" ", "-");
     var cid = category.replace(" ", "-");
-    var htmlControl = "<li class='option row clearfix' data-mid-id='" + oid + "'>";
-    htmlControl += "<div class='control half-column'>" + name + "</div>";
-    htmlControl += "<div class='control-value half-column'>";
+    var htmlControl = "<li class='list-item row' data-mid-id='" + oid + "'>";
+    htmlControl += "<div class='column-2'>" + name + "</div>";
+    htmlControl += "<div class='content-center column-2'>";
     htmlControl += "<input type='range' min='" + min + "' max='" + max + "' value='" + ((initialVal >= min && initialVal <= max) ? initialVal : min) + "' id='" + oid + "' step='" + intervalRes + "' oninput='" + (updateInRealTime ? "handleSliderChange" : "updateSliderOutput") + "(value, \"" + oid + "\"," + ID + ")'>";
     htmlControl += "<output for='" + oid + "' id='" + oid + "-output'>" + initialVal + "</output>";
     htmlControl += "</div>";
     htmlControl += "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div>";
     htmlControl += "</li>";
-    var cat = $(".option-category[data-category=" + cid + "]");
-    var elems = cat.nextUntil(".option-category");
-    if (elems.length == 0) {
-        cat.after(htmlControl);
-    } else {
-        elems.last().after(htmlControl);
-    }
-    var htmlDescriptor = "<div class='helper' data-mid-id='" + oid + "'>" + description + "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div></div>"
-    $(htmlDescriptor).appendTo(".options-helper-wrapper").hide();
-    optDescs = $(".options-helper-wrapper > div");
-    opts = $(".options > .option");
-    refreshDescControl();
+    placeControl(cid, htmlControl);
+    addDescription(oid, description);
 }
 
 /**
@@ -96,16 +63,16 @@ function generateSlider(ID, name, min, max, initialVal, intervalRes, updateInRea
  * vals - An array of values to exist in the combobox.
  * initialVal - The initial value of the combobox. Takes any individual value of vals.
  * updateInRealTime - Should changes to this control be reflected in-game immediately?
- * description - The description to be displayed, describing the controls effect on the game.
+ * description - The description to be displayed, describing the control's effect on the game.
  * category - The category to place the control under.
  */
-function generateCombo(ID, name, vals, initialVal, updateInRealTime, description, category) {
+function generateCombo(ID, name, vals, initialVal, updateInRealTime, category, description) {
     controls[ID] = initialVal;
     var oid = name.replace(" ", "-");
     var cid = category.replace(" ", "-");
-    var htmlControl = "<li class='option row clearfix' data-mid-id='" + oid + "'>";
-    htmlControl += "<div class='control half-column'>" + name + "</div>";
-    htmlControl += "<div class='control-value half-column'>";
+    var htmlControl = "<li class='list-item row' data-mid-id='" + oid + "'>";
+    htmlControl += "<div class='column-2'>" + name + "</div>";
+    htmlControl += "<div class='content-center column-2'>";
     htmlControl += "<select" + (updateInRealTime ? " onchange='handleComboChange(value, " + ID + ")'" : "") + ">";
     $.each(vals, function (i, v) {
         var vid = v.replace(" ", "-");
@@ -119,18 +86,8 @@ function generateCombo(ID, name, vals, initialVal, updateInRealTime, description
     htmlControl += "</div>";
     htmlControl += "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div>";
     htmlControl += "</li>";
-    var cat = $(".option-category[data-category=" + cid + "]");
-    var elems = cat.nextUntil(".option-category");
-    if (elems.length == 0) {
-        cat.after(htmlControl);
-    } else {
-        elems.last().after(htmlControl);
-    }
-    var htmlDescriptor = "<div class='helper' data-mid-id='" + oid + "'>" + description + "<div class='content-corner-top-right content-corner'></div><div class='content-corner-bottom-right content-corner'></div><div class='content-corner-top-left content-corner'></div><div class='content-corner-bottom-left content-corner'></div></div>"
-    $(htmlDescriptor).appendTo(".options-helper-wrapper").hide();
-    optDescs = $(".options-helper-wrapper > div");
-    opts = $(".options > .option");
-    refreshDescControl();
+    placeControl(cid, htmlControl);
+    addDescription(oid, description);
 }
 
 function updateSliderOutput(value, oid) {
@@ -156,33 +113,5 @@ function handleComboChange(value, ID) {
 function updateSettings() {
     $.each(controls, function (i, v) {
         App.updateSetting(i, v);
-    });
-}
-
-
-var opts;
-var optDescs;
-// Handle menu item descriptions.
-function refreshDescControl() {
-    opts.off('mouseover mouseout');
-    opts.on('mouseover', function () {
-        var midId = $(this).data("mid-id");
-        for (var x = 0; x < opts.length; ++x) {
-            if ($(optDescs[x]).data("mid-id") == midId) {
-                $(optDescs[x]).show(0);
-            } else {
-                $(optDescs[x]).stop(false, true);
-            }
-        }
-    })
-    opts.on('mouseout', function () {
-        var midId = $(this).data("mid-id");
-        for (var x = 0; x < opts.length; ++x) {
-            if ($(optDescs[x]).data("mid-id") == midId) {
-                $(optDescs[x]).hide(0);
-            } else {
-                $(optDescs[x]).stop(false, true);
-            }
-        }
     });
 }
