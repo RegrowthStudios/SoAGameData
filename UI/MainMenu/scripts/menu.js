9d9464f6e9e31b1e8f74662690e6a6f8cd16ded6
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2015-04-26 21:57:01 +0100

    changes to UI JS

--- UI/MainMenu/scripts/menu.js
+++ UI/MainMenu/scripts/menu.js
@@ -84,8 +84,10 @@ function loadNewPage(name, filePath) {
 }
 
 // Load controls for page.
-function initialisePage (controls) {
+function initializePage(controls) {
     $.each(controls, function (i, v) {
+        if ( v == "END" ) continue;
+        App.print(v);
         switch (v[0]) {
             case "click":
                 ListItemGenerator.generateClickable(v[1], v[2], v[3], v[4], v[5], v[6]);
